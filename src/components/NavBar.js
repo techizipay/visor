def3265f2ce9c504b8f3455f8df9af8aad4e66da
Tech Izipay <techizipay@gmail.com>
2022-01-11 19:37:10 -0500

    Primera version

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -113,77 +113,77 @@ export const NavBar = () => {
         // </Navbar.Collapse>
         // </Navbar> 
         <>
-        <AppBar position="static" style={{ background: '#ff1e16'}} > 
-      <Container maxWidth="xl">
-        <Toolbar disableGutters>
-          <Typography
-            variant="h6"
-            noWrap
-            component="div"
-            sx={{ mr: 2, display: { xs: 'none', md: 'flex' } }}
-          >
-            <img src='https://www.izipay.pe/_nuxt/dist/img/logo.8e6aa05.png' width='100px' height='50px' /> 
-          </Typography>
+        <AppBar position="static" className="contenedor" style={{ background: '#ff1e16'}} > 
+          <Container maxWidth="xl">
+            <Toolbar disableGutters>
+              <Typography
+                variant="h6"
+                noWrap
+                component="div"
+                sx={{ mr: 2, display: { xs: 'none', md: 'flex' } }}
+              >
+                <img src='https://www.izipay.pe/_nuxt/dist/img/logo.8e6aa05.png' width='100px' height='50px' /> 
+              </Typography>
 
-          <Box sx={{ flexGrow: 1, display: { xs: 'flex', md: 'none' } }}>
-            <IconButton
-              size="large"
-              aria-label="account of current user"
-              aria-controls="menu-appbar"
-              aria-haspopup="true"
-              onClick={handleOpenNavMenu}
-              color="inherit"
-            >
-              <MenuIcon />
-            </IconButton>
-            <Menu
-              id="menu-appbar"
-              anchorEl={anchorElNav}
-              anchorOrigin={{
-                vertical: 'bottom',
-                horizontal: 'left',
-              }}
-              keepMounted
-              transformOrigin={{
-                vertical: 'top',
-                horizontal: 'left',
-              }}
-              open={Boolean(anchorElNav)}
-              onClose={handleCloseNavMenu}
-              sx={{
-                display: { xs: 'block', md: 'none' },
-              }}
-            >
-              {pages.map((page) => (
-                <MenuItem key={page} onClick={handleCloseNavMenu}>
-                  <Typography textAlign="center">{page}</Typography>
-                </MenuItem>
-              ))}
-            </Menu>
-          </Box>
-          <Typography
-            variant="h6"
-            noWrap
-            component="div"
-            sx={{ flexGrow: 1, display: { xs: 'flex', md: 'none' } }}
-          >
-            <img src='https://www.izipay.pe/_nuxt/dist/img/logo.8e6aa05.png' width='100px' height='50px' /> 
-          </Typography>
+              <Box sx={{ flexGrow: 1, display: { xs: 'flex', md: 'none' } }}>
+                <IconButton
+                  size="large"
+                  aria-label="account of current user"
+                  aria-controls="menu-appbar"
+                  aria-haspopup="true"
+                  onClick={handleOpenNavMenu}
+                  color="inherit"
+                >
+                  <MenuIcon />
+                </IconButton>
+                <Menu
+                  id="menu-appbar"
+                  anchorEl={anchorElNav}
+                  anchorOrigin={{
+                    vertical: 'bottom',
+                    horizontal: 'left',
+                  }}
+                  keepMounted
+                  transformOrigin={{
+                    vertical: 'top',
+                    horizontal: 'left',
+                  }}
+                  open={Boolean(anchorElNav)}
+                  onClose={handleCloseNavMenu}
+                  sx={{
+                    display: { xs: 'block', md: 'none' },
+                  }}
+                >
+                  {pages.map((page) => (
+                    <MenuItem key={page} onClick={handleCloseNavMenu}>
+                      <Typography textAlign="center">{page}</Typography>
+                    </MenuItem>
+                  ))}
+                </Menu>
+              </Box>
+              <Typography
+                variant="h6"
+                noWrap
+                component="div"
+                sx={{ flexGrow: 1, display: { xs: 'flex', md: 'none' } }}
+              >
+                <img src='https://www.izipay.pe/_nuxt/dist/img/logo.8e6aa05.png' width='100px' height='50px' /> 
+              </Typography>
 
-          <Box sx={{ flexGrow: 1, display: { xs: 'none', md: 'flex' }, justifyContent:'flex-end' }}>
-             
-                <Nav className="ml-auto pr-2">     
-                        <Link to="" onClick={(e) => handleClickConf(e)} className='d-flex flex-column my-auto justify-content-center sinsombra' style={{textDecoration: 'none'}}> 
-                                <i className="fas fa-cog fa-2x icon"/>
-                                <span className="icon">IPN</span>    
-                        </Link>   
-                </Nav> 
-          </Box>
- 
- 
-        </Toolbar>
-      </Container>
-    </AppBar>
+              <Box sx={{ flexGrow: 1, display: { xs: 'none', md: 'flex' }, justifyContent:'flex-end' }}>
+                
+                    <Nav className="ml-auto pr-2">     
+                            <Link to="" onClick={(e) => handleClickConf(e)} className='d-flex flex-column my-auto justify-content-center sinsombra' style={{textDecoration: 'none'}}> 
+                                    <i className="fas fa-cog fa-2x icon"/>
+                                    <span className="icon">IPN</span>    
+                            </Link>   
+                    </Nav> 
+              </Box>
+    
+    
+            </Toolbar>
+          </Container>
+        </AppBar>
 
 
     
