a49cd4e009c7e9c8a2f60784c2c04c7b528433e0
Tech Izipay <techizipay@gmail.com>
2022-01-14 16:25:17 -0500

    Agregar formulario de pago

--- src/views/ConfirmationView.js
+++ src/views/ConfirmationView.js
@@ -1,66 +1,95 @@
-import React, {useContext, useState, useEffect} from 'react';
-import {Link} from "react-router-dom";
-import { PayContext } from '../context/payContext';
-import {FormConfirmation} from '../components/MiCuentaWeb/FormIncrustado/FormConfirmation';
-import '../components/css/Confirmation.css'; 
-
-export const ConfirmationView = () => { 
-    const { order, limpiarOrder } = useContext(PayContext); 
   
-    const [name, setName] = useState('');
-    const [email, setEmail] = useState('');
-    const [address, setAddress] = useState(''); 
-    const [total, setTotal] = useState(0);
-
-    const getData = () =>{  
+    import React, {useContext, useState, useEffect} from 'react';
+    import {Link} from "react-router-dom";
+    import { PayContext } from '../context/payContext';
+    import Modal from 'react-bootstrap/Modal'; 
+    import Button from 'react-bootstrap/Button';
+    import {FormConfirmation} from '../components/MiCuentaWeb/FormIncrustado/FormConfirmation';
+    import '../components/css/Confirmation.css'; 
     
-        limpiarOrder(); 
-    }
-  
-    useEffect(() => {
-        getData();
-    }, [])
- 
-    return (
-        <div className="d-flex justify-content-center">
-            <div style={{width: '600px', marginTop: '5rem', marginBottom: '1rem'}}>
-
-            <div className="card mt-3">
+    export const ConfirmationView = () => { 
+        const { order, limpiarOrder } = useContext(PayContext); 
+        const [modalShow, setModalShow] = React.useState(false); 
+    
+        const getData = () =>{  
+        
+            limpiarOrder(); 
+        }
+      
+        useEffect(() => {
+            getData();
+        }, [])
+     
+     
+        return (
+            <div className="d-flex justify-content-center">
+                <div style={{width: '600px', marginTop: '5rem', marginBottom: '1rem'}}>
+    
+                <div className="card mt-3">
+                    <div className="d-flex justify-content-center clogo">
+                        <img src="https://upload.wikimedia.org/wikipedia/commons/e/e0/Check_green_icon.svg" alt="..." className="confirmation"/>
+                    </div>
+                    <div className="card-body">
+                        <h3 className="card-title">
+                            Gracias por su pedido!
+                        </h3>
+    
+                        <div>
+                            {/* <h5>Hola {name}</h5> */}
+                            {/* <p>Gracias por comprar con nosotros. Te enviaremos una confirmación a {email} cuando tus artículos se envíen.</p> */}
+                            <p>Gracias por comprar con nosotros. Te enviaremos una confirmación cuando tus artículos se envíen.</p>
+                        </div>
+      
+    
+                        <div>
+                            <p>Esperamos volver a verte pronto.</p>
+                        </div>
+    
+                        <Button variant="primary" onClick={() => setModalShow(true)}>
+                            Ver Objeto de Respuesta
+                        </Button>
+                        <MyVerticallyCenteredModal
+                            show={modalShow}
+                            onHide={() => setModalShow(false)}
+                        />
+    
+                        <Link className="btn btn-primary btn-block" to="/home">Seguir Comprando</Link>
+                        <br></br>
+    
+                        
+                        
+                    </div>
+                </div>
+    
                 <div className="d-flex justify-content-center clogo">
-                    <img src="https://upload.wikimedia.org/wikipedia/commons/e/e0/Check_green_icon.svg" alt="..." className="confirmation"/>
+                    <img src="https://www.izipay.pe/_nuxt/dist/img/logo.8e6aa05.png" alt="..."  className="logo"/> 
                 </div>
-                <div className="card-body">
-                    <h3 className="card-title">
-                        Gracias por su pedido!
-                    </h3>
-
-                    <div>
-                        {/* <h5>Hola {name}</h5> */}
-                        {/* <p>Gracias por comprar con nosotros. Te enviaremos una confirmación a {email} cuando tus artículos se envíen.</p> */}
-                        <p>Gracias por comprar con nosotros. Te enviaremos una confirmación cuando tus artículos se envíen.</p>
-                    </div>
-  
-
-                    <div>
-                        <p>Esperamos volver a verte pronto.</p>
-                    </div>
-
-                    <Link className="btn btn-primary btn-block" to="/home">Seguir Comprando</Link>
-                    <br></br>
-
-                    <FormConfirmation/>
-                    
-                </div>
-            </div>
-
-            <div className="d-flex justify-content-center clogo">
-                <img src="https://www.izipay.pe/_nuxt/dist/img/logo.8e6aa05.png" alt="..."  className="logo"/> 
-            </div>
-
-            </div>
-        </div>
-    )
     
-}
-
- 
+                </div>
+            </div>
+        )
+        
+    }
+    
+    function MyVerticallyCenteredModal(props) {
+        return (
+          <Modal
+            {...props}
+            size="lg"
+            aria-labelledby="contained-modal-title-vcenter"
+            centered
+          >
+            <Modal.Header closeButton>
+              <Modal.Title id="contained-modal-title-vcenter">
+               Objeto Payment
+              </Modal.Title>
+            </Modal.Header>
+            <Modal.Body>
+                <FormConfirmation/>
+            </Modal.Body>
+            <Modal.Footer>
+              <Button onClick={props.onHide}>Close</Button>
+            </Modal.Footer>
+          </Modal>
+        );
+      }
